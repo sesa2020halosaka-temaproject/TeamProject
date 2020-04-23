c316ee6cae5d6fd2841504824ad80eb3973a6623
RyuichiMasuyama <57240248+RyuichiMasuyama@users.noreply.github.com>
2020-04-23 14:46:27 +0900

    状態遷移の多めの汎用クラス作成 プレイヤー、小人のスクリプト、プレハブの作成

---
+++ Assets/Scripts/Stage/ObjectVer2/Player/PlayerVer2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// プレイヤーのスクリプト
+
+namespace TeamProject
+{
+    public class PlayerVer2 : System.TransitionObject
+    {
+        enum TRANSITION
+        {
+            None,   // 何もしない(使わないが一応)
+            Move,   // 移動
+            Choice, // 選択
+            MAX,    // MAX(使用しない)
+        }
+
+        // 移動速度
+        [SerializeField]
+        [Header("移動速度調節")]
+        [Range(1.0f,20.0f)]
+        private float moveSpeed = 13.0f;
+
+        // 段差判定の精度
+        [SerializeField]
+        [Header("段差判定の精度(プログラマーのみ変更許可)")]
+        [Range(0.01f, 1.0f)]
+        private float stepJudgeAccuracy;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // 移動
+        private void Move()
+        {
+
+        }
+
+        // 選択
+        private void Choice()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Stage/ObjectVer2/TransitionObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamProject
+{
+    namespace System
+    {
+        // デリゲート宣言
+        public delegate void Function();
+
+        // 状態遷移が頻繁に起こるオブジェクトの基底クラス
+        // 基底クラスなのでScriptをゲームオブジェクトに貼りつけて
+        // 使用はしない
+        public class TransitionObject : MonoBehaviour
+        {
+            // 設定できるFunctionの最大数
+            private const uint MAX_FUNTION_NUM = 999;
+
+            private Function[] function;
+
+            private uint maxFunctionNum = 0;
+
+            private uint nowFunctionNum = MAX_FUNTION_NUM;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                // 設定されていないので無視
+                if (nowFunctionNum == MAX_FUNTION_NUM) return;
+
+                // 設定されているFunctionを実行
+                function[nowFunctionNum]();
+            }
+            
+            // Functionの最大数を設定する
+            public void SetMaxFunctionSize(uint _maxFunctionNum) {
+                function = new Function[_maxFunctionNum];
+                maxFunctionNum = _maxFunctionNum;
+            }
+
+            // Functionを設定する
+            public bool CreateFunction(uint _functionNum, Function _setFunction)
+            {
+                // 配列の最大数より大きければfalseを返す
+                if (maxFunctionNum < _functionNum) return false;
+                
+                function[_functionNum] = _setFunction;
+
+                return true;
+            }
+
+            public bool SetFunction(uint _functionNum)
+            {
+                // 配列の最大数より大きければfalseを返す
+                if (maxFunctionNum < _functionNum) return false;
+
+                nowFunctionNum = _functionNum;
+
+                return true;
+            }
+        }
+    }
+}
