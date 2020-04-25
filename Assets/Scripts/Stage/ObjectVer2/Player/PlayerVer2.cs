168c6846dc83ccb84bba03feffc6fdbeea21c503
RyuichiMasuyama <57240248+RyuichiMasuyama@users.noreply.github.com>
2020-04-25 18:45:19 +0900

    PlayerVer2の処理追加

--- Assets/Scripts/Stage/ObjectVer2/Player/PlayerVer2.cs
+++ Assets/Scripts/Stage/ObjectVer2/Player/PlayerVer2.cs
@@ -16,6 +16,20 @@ namespace TeamProject
             MAX,        // MAX(使用しない)
         }
 
+        enum DIRECTION
+        {
+            RIGHT,
+            LEFT,
+            TOP,
+            BACK,
+            MAX
+        }
+
+        private class ConversPosition{
+            public GameObject befor;
+            public Vector3 convers;
+        }
+
         // 移動速度
         [SerializeField]
         [Header("移動速度調節")]
@@ -39,7 +53,7 @@ namespace TeamProject
         private GameObject pickArrowPrefab;
         
         // Choiceできるオブジェクトのリスト
-        private List<GameObject> choiceObjectList;
+        private List<ConversPosition> choiceObjectList;
 
         // カメラオブジェクト
         private GameObject cameraObject;
@@ -103,14 +117,66 @@ namespace TeamProject
         // 選択
         private void Choice()
         {
+            ConversPosition[] direction = new ConversPosition[(uint)DIRECTION.MAX];
 
+            // 初期位置設定
+            foreach (var itr in direction) itr.convers = choicePosition;
+
+            // カメラとの位置情報割り出し
+            foreach (var itr in choiceObjectList)
+            {
+                var convPos = CameraConversion(itr.befor.transform.position);
+
+                itr.convers = convPos;
+            }
+            
+
+            // 左右上下のオブジェクト割り出し
+            foreach (var itr in choiceObjectList)
+            {
+                // 差分割り出し
+                var diff = itr.convers - choicePosition;
+
+                // 左
+                if (diff.x < 0f)
+                {
+                    if (diff.x< direction[(uint)DIRECTION.LEFT].convers.x) {
+                        direction[(uint)DIRECTION.LEFT] = itr;
+                    }
+                }
+                else if (0f < diff.x)   // 右
+                {
+                    if (diff.x < direction[(uint)DIRECTION.RIGHT].convers.x)
+                    {
+                        direction[(uint)DIRECTION.RIGHT] = itr;
+                    }
+                }
+                // 後
+                if (diff.z < 0f)
+                {
+                    if (diff.x < direction[(uint)DIRECTION.BACK].convers.x)
+                    {
+                        direction[(uint)DIRECTION.BACK] = itr;
+                    }
+                }
+                else if (0f < diff.z)   // 前
+                {
+                    if (diff.x < direction[(uint)DIRECTION.TOP].convers.x)
+                    {
+                        direction[(uint)DIRECTION.TOP] = itr;
+                    }
+                }
+
+                if (direcPos[(uint)DIRECTION.RIGHT].convers < itr.convers)
+                    direction[(uint)DIRECTION.RIGHT] = itr;
+            }
         }
 
         // 選択の配列取得
         private void GetChoice()
         {
             // 配列の生成
-            choiceObjectList = new List<GameObject>();
+            choiceObjectList = new List<ConversPosition>();
 
             // 配列の取得
             var objectArray = GameObject.FindGameObjectsWithTag("ChoiceObject");
@@ -118,8 +184,9 @@ namespace TeamProject
             // 配列のオブジェクトの親をListに追加
             foreach(var itr in objectArray)
             {
+                ConversPosition parentObject = new ConversPosition();
                 // 親を取得
-                var parentObject = itr.transform.root.gameObject;
+                parentObject.befor = itr.transform.root.gameObject;
 
                 // リストに追加
                 choiceObjectList.Add(parentObject);
@@ -133,6 +200,18 @@ namespace TeamProject
 
             // 次ループから選択の関数へ
             SetFunction((uint)TRANSITION.Choice);
+        }
+
+        // カメラの位置変換
+        private Vector3 CameraConversion(Vector3 _pos)
+        {
+            // 長さ割出
+            var length = Vector3.Distance(choicePosition, _pos);
+
+            var cameraRotY = cameraObject.transform.rotation.eulerAngles.y;
+
+            //　ユニットの位置をターゲットの位置からの角度にベクトルをかけて計算
+            return choicePosition + Quaternion.Euler(0f, cameraRotY, 0f) * new Vector3(0f, 0f, length);
         }
     }
 }
