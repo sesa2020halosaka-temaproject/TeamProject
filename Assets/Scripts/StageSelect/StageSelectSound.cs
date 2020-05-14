815d8e910bca2b43fd75479e130c9597f34efaf7
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2020-05-14 23:04:35 +0900

    ・ステージセレクトのワールド移動時のBGM切り替え。（修正版）

--- Assets/Scripts/StageSelect/StageSelectSound.cs
+++ Assets/Scripts/StageSelect/StageSelectSound.cs
@@ -9,15 +9,52 @@ namespace TeamProject
     //ステージセレクトのサウンドを担当します。
     public class StageSelectSound : MonoBehaviour
     {
-        public string m_BGM_Summer;//BGM名１
-        public string m_BGM_Fall;//BGM名２
-        public string m_BGM_Winter;//BGM名３
-        public string m_BGM_Spring;//BGM名４
+        public float m_CrossFadeTime;//クロスフェードする時間
+        public float m_SEVolume;
         public string m_Ambient_Name;//環境音の名前
-         public float m_CrossFadeTime;//クロスフェードする時間
-         public float Volume;
+        public bool m_ChangeBGM_Flag = false;//
+        public float[] m_Volume = new float[(int)WORLD_NO.ALL_WORLD];//
+        public float[] m_Switching = new float[(int)WORLD_NO.ALL_WORLD];//
+
+        [SerializeField]
+        private AudioClip[] AudioClip = new AudioClip[(int)WORLD_NO.ALL_WORLD];
+        [SerializeField]
+        private AudioSource[] AudioSource = new AudioSource[(int)WORLD_NO.ALL_WORLD];
+
+
         //=====================================================
         //関数ここから
+        private void Awake()
+        {
+            string[] AudioName = {
+                BGMPath.BGM_STAGE_SELECT_SUMMER,
+                BGMPath.BGM_STAGE_SELECT_FALL,
+                BGMPath.BGM_STAGE_SELECT_WINTER,
+                BGMPath.BGM_STAGE_SELECT_SPRING
+            };
+            //BGMを取得
+            for (int i = 0; i < (int)WORLD_NO.ALL_WORLD; i++)
+            {
+                AudioClip[i] = Resources.Load<AudioClip>(AudioName[i]);
+                Debug.Log(AudioClip[i] + " not found");
+
+                if (AudioClip[i] == null)
+                {
+                    Debug.LogError(AudioClip[i] + " not found");
+                }
+                AudioSource[i] = default;
+                AudioSource[i] = gameObject.AddComponent<AudioSource>();
+                AudioSource[i].loop = true;
+                AudioSource[i].spatialBlend = 0f;
+
+                m_Volume[i] = 0.0f;
+                AudioSource[i].volume = m_Volume[i];
+                AudioSource[i].clip = AudioClip[i];
+            }
+
+
+
+        }
         // Start is called before the first frame update
         void Start()
         {
@@ -27,69 +64,145 @@ namespace TeamProject
         // Update is called once per frame
         void Update()
         {
-
+            ChangeBGMVolume();
         }
 
-
-        //現在のワールドからBGMを設定する
-        public void SetCurrentWorldBGM()
+        public void SetSwitching()
         {
-            //BGMスタート
-            switch (StageStatusManager.Instance.CurrentWorld)
+            //まずは全てのボリューム増減値をゼロに
+            for (int i = 0; i < (int)WORLD_NO.ALL_WORLD; i++)
             {
-                case (int)WORLD_NO.W1://ワールド１：夏
-                    m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_SUMMER;
+                m_Switching[i] = 0.0f;
+
+            }
+            //指定されたワールド前後のBGMのボリュームの増減をセットする
+            switch (StageChangeManager.GetStageChangeKey())
+            {
+                //前のワールドへ
+                case StageChangeManager.STAGE_CHANGE_KEY.LEFT:
+                    m_Switching[StageStatusManager.Instance.CurrentWorld] = -1.0f;
+                    m_Switching[StageStatusManager.Instance.PrevWorld] = 1.0f;
                     break;
-                case (int)WORLD_NO.W2://ワールド２：秋
-                    m_BGM_Summer = BGMPath.BGM_GAME_FALL;
+                //次のワールドへ
+                case StageChangeManager.STAGE_CHANGE_KEY.RIGHT:
+                    m_Switching[StageStatusManager.Instance.CurrentWorld] = -1.0f;
+                    m_Switching[StageStatusManager.Instance.NextWorld] = 1.0f;
                     break;
-                case (int)WORLD_NO.W3://ワールド３：冬
-                    m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_WINTER;
-                    break;
-                case (int)WORLD_NO.W4://ワールド４：春
-                    m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_SPRING;
+                case StageChangeManager.STAGE_CHANGE_KEY.ALL:
+                default:
+                    Debug.Log("状態が違います。");
                     break;
             }
 
         }
-        //現在のワールドから次ワールドのBGMを設定する
-        public void SetNextWorldBGM()
+
+        //音量を変更する
+        public void ChangeBGMVolume()
         {
-            //BGMスタート
+            if (m_ChangeBGM_Flag)
+            {
+                float DeltaTime = Time.deltaTime;
+
+                //まずは全てのボリューム増減値をゼロに
+                for (int i = 0; i < (int)WORLD_NO.ALL_WORLD; i++)
+                {
+                    m_Volume[i] += m_Switching[i] * DeltaTime / m_CrossFadeTime;
+
+                    if (m_Volume[i] >= 1.0f)
+                    {
+                        m_Volume[i] = 1.0f;
+                        int prev = Rooping(i - 1, 0, 3);
+                        int next = Rooping(i + 1, 0, 3);
+
+                        if (m_Volume[prev] <= 0.0f || m_Volume[next] <= 0.0f)
+                        {
+                            m_Volume[prev] = 0.0f;
+                            m_Volume[next] = 0.0f;
+                            ChangeBGMFlagOFF();
+                        }
+                    }
+                    AudioSource[i].volume = m_Volume[i];
+                }
+
+
+
+            }
+
+        }
+        //最大最小を超えた時の処理
+        public int Rooping(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                value = max;
+            }
+            else if (value > max)
+            {
+                value = min;
+            }
+            return value;
+        }
+
+
+        //現在のワールドから環境音を設定する
+        public void SetCurrentWorldAmbient()
+        {
+            //環境音セット
             switch (StageStatusManager.Instance.CurrentWorld)
             {
                 case (int)WORLD_NO.W1://ワールド１：夏
-                     m_BGM_Summer = BGMPath.BGM_GAME_FALL;//秋
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_SUMMER;
                     break;
                 case (int)WORLD_NO.W2://ワールド２：秋
-                     m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_WINTER;//冬
-                   break;
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_FALL;
+                    break;
                 case (int)WORLD_NO.W3://ワールド３：冬
-                     m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_SPRING;//春
-                   break;
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_WINTER;
+                    break;
                 case (int)WORLD_NO.W4://ワールド４：春
-                   m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_SUMMER;//夏
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_SPRING;
                     break;
             }
 
         }
-        //現在のワールドから前ワールドのBGMを設定する
-        public void SetPrevWorldBGM()
+        //現在のワールドから次ワールドの環境音を設定する
+        public void SetNextWorldAmbient()
         {
-            //BGMスタート
+            //環境音セット
             switch (StageStatusManager.Instance.CurrentWorld)
             {
                 case (int)WORLD_NO.W1://ワールド１：夏
-                     m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_SPRING;//春
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_FALL;//秋
                     break;
                 case (int)WORLD_NO.W2://ワールド２：秋
-                   m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_SUMMER;//夏
-                   break;
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_WINTER;//冬
+                    break;
                 case (int)WORLD_NO.W3://ワールド３：冬
-                     m_BGM_Summer = BGMPath.BGM_GAME_FALL;//秋
-                   break;
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_SPRING;//春
+                    break;
                 case (int)WORLD_NO.W4://ワールド４：春
-                     m_BGM_Summer = BGMPath.BGM_STAGE_SELECT_WINTER;//冬
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_SUMMER;//夏
+                    break;
+            }
+
+        }
+        //現在のワールドから前ワールドの環境音を設定する
+        public void SetPrevWorldAmbient()
+        {
+            //環境音セット
+            switch (StageStatusManager.Instance.CurrentWorld)
+            {
+                case (int)WORLD_NO.W1://ワールド１：夏
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_SPRING;//春
+                    break;
+                case (int)WORLD_NO.W2://ワールド２：秋
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_SUMMER;//夏
+                    break;
+                case (int)WORLD_NO.W3://ワールド３：冬
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_FALL;//秋
+                    break;
+                case (int)WORLD_NO.W4://ワールド４：春
+                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT_WINTER;//冬
                     break;
             }
 
@@ -97,38 +210,65 @@ namespace TeamProject
         //開始時のBGM設定
         public void StageSelectStartBGM()
         {
-            SetCurrentWorldBGM();
-                    m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT;
-            //BGMスタート
-                    BGMManager.Instance.Play(m_BGM_Summer);
-                    //水の音追加
-                    BGMManager.Instance.Play(m_Ambient_Name, volumeRate: Volume, delay: 0.0f, isLoop: true, allowsDuplicate: true);
-                    BGMManager.Instance.FadeIn(m_Ambient_Name, duration: 2.0f);
+            //現在のワールドのボリュームを1にする
+            m_Volume[StageStatusManager.Instance.CurrentWorld] = 1.0f;
+
+            for (int i = 0; i < (int)WORLD_NO.ALL_WORLD; i++)
+            {
+                AudioSource[i].volume = m_Volume[i];
+                //BGMスタート
+                AudioSource[i].Play();
+            }
+            //SetCurrentWorldBGM();
+
+            //環境音追加
+            //SEセット
+            SetCurrentWorldAmbient();
+            BGMManager.Instance.Play(m_Ambient_Name, volumeRate: m_SEVolume, delay: 0.0f, isLoop: true, allowsDuplicate: true);
+            BGMManager.Instance.FadeIn(m_Ambient_Name, duration: 2.0f);
         }//StageSelectStartBGM() END
 
 
+        //フラグON
+        public void ChangeBGMFlagON()
+        {
+            m_ChangeBGM_Flag = true;
+        }
+        //フラグOFF
+        public void ChangeBGMFlagOFF()
+        {
+            m_ChangeBGM_Flag = false;
+        }
+
         public void CrossFade()
         {
+            //
+            SetSwitching();
+            //
+            ChangeBGMFlagON();
+
+            //環境音のセット
             switch (StageChangeManager.GetStageChangeKey())
             {
                 case StageChangeManager.STAGE_CHANGE_KEY.LEFT:
-                    SetPrevWorldBGM();
+                    SetPrevWorldAmbient();
                     break;
                 case StageChangeManager.STAGE_CHANGE_KEY.RIGHT:
-                    SetNextWorldBGM();
+                    SetNextWorldAmbient();
                     break;
                 case StageChangeManager.STAGE_CHANGE_KEY.ALL:
                 default:
                     Debug.Log("状態が違います。");
                     break;
             }
-            m_Ambient_Name = SEPath.SE_AMB_STAGE_SELECT;
-            string m_Ambient_Name2 = SEPath.SE_AMB_STAGE_SELECT;
-            //BGMSwitcher.FadeOutAndFadeIn(m_BGM_Name, fadeInDuration: 2.5f, fadeOutDuration: 2.5f);
-            BGMSwitcher.CrossFade(m_BGM_Summer, fadeDuration: m_CrossFadeTime);
+            //環境音のクロスフェード
+             BGMSwitcher.CrossFade(m_Ambient_Name, fadeDuration: m_CrossFadeTime);
+
+
+           //BGMSwitcher.FadeOutAndFadeIn(m_BGM_Name, fadeInDuration: 2.5f, fadeOutDuration: 2.5f);
             //BGMManager.Instance.FadeOut(m_Ambient_Name, duration: m_CrossFadeTime);
-            BGMManager.Instance.Play(m_Ambient_Name2, volumeRate: Volume, delay: 0.0f, isLoop: true, allowsDuplicate: true);
-            BGMManager.Instance.FadeIn(m_Ambient_Name2, duration: m_CrossFadeTime);
+            //BGMManager.Instance.Play(m_Ambient_Name2, volumeRate: Volume, delay: 0.0f, isLoop: true, allowsDuplicate: true);
+            //BGMManager.Instance.FadeIn(m_Ambient_Name2, duration: m_CrossFadeTime);
         }
     }//public class StageSelectSound : MonoBehaviour END
 }//namespace END
