c4e6bda810a2f4d5bad49777986df8e291ef60d8
RyuichiMasuyama <57240248+RyuichiMasuyama@users.noreply.github.com>
2020-05-31 00:15:12 +0900

    氷のアニメーション追加(透明化処理)

--- Assets/Scripts/Stage/Gimmick/IceGimmick.cs
+++ Assets/Scripts/Stage/Gimmick/IceGimmick.cs
@@ -17,11 +17,13 @@ namespace TeamProject
         [SerializeField]
         private uint fallNum = 1;
 
+        private Animator iceAnime;
+
         private void Start()
         {
             coll = GetComponent<Collider>();
 
-            Debug.Log(coll+"gporekaopgkrepogokerakgpoaker ogkpaeok");
+            iceAnime = GetComponentInChildren<Animator>();
         }
         
         // 壊れるかどうかのJudge
@@ -36,6 +38,7 @@ namespace TeamProject
             SEManager.Instance.Play(SEPath.SE_BROKEN_ICE);
             coll.enabled = false;
             collChild.enabled = false;
+            iceAnime.SetTrigger("On");
         }
 
         // オブジェクト自体を消す処理
