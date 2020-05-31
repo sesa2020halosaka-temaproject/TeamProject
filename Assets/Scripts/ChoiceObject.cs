a3fe5b7b6e4302d24e1452cf58dfa9b629a5d6b3
RyuichiMasuyama <57240248+RyuichiMasuyama@users.noreply.github.com>
2020-05-31 10:53:18 +0900

    フロア管理系

---
+++ Assets/Scripts/ChoiceObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceObject : MonoBehaviour
+{
+    [SerializeField, Range(1, 5)]
+    [Header("階層の入力")]
+    private int floor = 1;
+
+    public int Floor { get { return floor; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
