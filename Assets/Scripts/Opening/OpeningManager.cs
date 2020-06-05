c1ae6d823bd121fb2851cc7bfb37332acba5da33
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2020-06-05 13:46:23 +0900

    ・オープニングシーンのスキップボタンUIを右下に配置、時間で消える処理の追加。 ・オープニングシーンのポーズ画面を廃止。

--- Assets/Scripts/Opening/OpeningManager.cs
+++ Assets/Scripts/Opening/OpeningManager.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 using UnityEngine.UI;
+using System;
 
 namespace TeamProject
 {
     //ワールド選択可能表示矢印用クラス
     public class OpeningManager : MonoBehaviour
     {
-
+        [Header("SKIPアイコンが消えるまでの時間"), Range(0.0f, 37.0f)]
+        public float m_InvisibleTime;
+        private float m_TimeCount = 0;
+        private bool m_InvisibleFlag = false;
         public VideoClip m_VideoClip;
         public VideoPlayer m_VideoPlayer;
+        public GameObject m_SkipButtonObj;
         public GameObject m_PausePanelObj;
-       // private RawImage rawImage;
+        // private RawImage rawImage;
 
         public enum MOVIE_STATE
         {
             START = 0,//ムービー開始前
-            WATCH ,//視聴中
+            WATCH,//視聴中
             PAUSE,//ポーズ中
             SCENE_CHANGE,//シーン遷移
             ALLSTATE//
@@ -30,21 +35,27 @@ namespace TeamProject
         void Start()
         {
             m_MovieState = MOVIE_STATE.START;
+            //Skipボタン用UIのオブジェクトの取得
+            m_SkipButtonObj = GameObject.Find("SkipObj");
+            m_SkipButtonObj.SetActive(true);
+
             //ポーズ用パネルオブジェクトの設定
             m_PausePanelObj = GameObject.Find("PAUSE_Panel");
             m_PausePanelObj.SetActive(false);
 
+
+            //----------------------------------------------
             //ムービー系統の設定
             if (m_VideoPlayer == null)
             {
                 Debug.Log("VideoPlayerがセットされていません！");
                 m_VideoPlayer = this.GetComponent<VideoPlayer>();
-                Debug.Log("だから" + m_VideoPlayer.name+                    "をセットしました！");
+                Debug.Log("だから" + m_VideoPlayer.name + "をセットしました！");
             }
             if (m_VideoClip == null)
             {
                 Debug.LogError("VideoClipがセットされていません！");
-                }
+            }
 
             // 即再生されるのを防ぐ.
             m_VideoPlayer.playOnAwake = false;
@@ -68,7 +79,29 @@ namespace TeamProject
         // Update is called once per frame
         void Update()
         {
+            MoveStateUpdate();
             //Debug.LogError(m_VideoPlayer.isPlaying + "！");
+        }//Update() END
+
+        //SkipボタンUI非表示用カウント関数
+        private void InvisibleTimeUpdate()
+        {
+            if (!m_InvisibleFlag)
+            {
+                m_TimeCount += Time.deltaTime;
+                if (m_TimeCount > m_InvisibleTime)
+                {
+                    m_TimeCount = 0;
+                    m_SkipButtonObj.SetActive(false);
+                    m_InvisibleFlag = true;
+                }
+
+            }
+        }
+
+        //MoveState用更新関数
+        private void MoveStateUpdate()
+        {
             switch (m_MovieState)
             {
                 case MOVIE_STATE.START:
@@ -89,28 +122,19 @@ namespace TeamProject
                 default:
                     break;
             }
-
-        }//Update() END
+        }
 
         //スタート時の更新処理
         private void StateStartUpdate()
         {
-
-            if (m_VideoPlayer.isPlaying)
-            {
-                m_MovieState = MOVIE_STATE.WATCH;
-
-            }
-            else
-            {
-                Debug.Log("まだムービーは" + m_VideoPlayer.isPlaying + "だよ！");
-
-
-            }
+            if (m_VideoPlayer.isPlaying) { m_MovieState = MOVIE_STATE.WATCH; }
+            else { Debug.Log("まだムービーは" + m_VideoPlayer.isPlaying + "だよ！"); }
         }
 
         private void StateWatchUpdate()
         {
+            InvisibleTimeUpdate();
+
             if (!m_VideoPlayer.isPlaying)
             {
                 //ムービーが終わっていれば
@@ -121,11 +145,17 @@ namespace TeamProject
             {
                 if (InputManager.InputManager.Instance.GetKeyUp(InputManager.ButtunCode.Menu))
                 {
-                    m_MovieState = MOVIE_STATE.PAUSE;
-                    m_PausePanelObj.SetActive(true);
+                    //STARTボタンでSKIPする
+                    m_MovieState = MOVIE_STATE.SCENE_CHANGE;
+
+
+                    //m_MovieState = MOVIE_STATE.PAUSE;
+                    //m_PausePanelObj.SetActive(true);
                 }
             }
         }
+
+        //廃止されました
         //ポーズ時の更新処理
         private void StatePauseUpdate()
         {
@@ -135,7 +165,7 @@ namespace TeamProject
             }
             else
             {
-                if ((InputManager.InputManager.Instance.GetKeyUp(InputManager.ButtunCode.Menu)) 
+                if ((InputManager.InputManager.Instance.GetKeyUp(InputManager.ButtunCode.Menu))
                     || (InputManager.InputManager.Instance.GetKeyUp(InputManager.ButtunCode.A)))
                 {
                     //STARTボタンorAボタン
@@ -151,7 +181,8 @@ namespace TeamProject
                     m_MovieState = MOVIE_STATE.SCENE_CHANGE;
                 }
             }
-        }
+        }//StatePauseUpdate()   END
+
         //シーン遷移時の更新処理
         private void StateSceneChangeUpdate()
         {
