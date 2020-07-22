13aa4372840d2e451a1a797e774f20b5135967c9
takenakahan <61533682+takenakahan@users.noreply.github.com>
2020-07-23 03:06:40 +0900

    Revert "Revert "ポストエフェクトや3-3に雪を置いたりOP差し替えを行いました""
    This reverts commit 5c6f71e85e9b2ac2d3e6070b1b0d56360167f93e.

---
+++ Assets/BrackBox/aassd/FadeOil.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeOil : MonoBehaviour
+{
+    GameObject FlagManager;
+    TeamProject.StageStatusManager StageStatusManager;
+    RawImage rend;
+    public bool flg;
+    public float time = 10;
+    public float radius = 10;
+
+    void Start()
+    {
+        //フラグ管理にアクセス
+        FlagManager = GameObject.Find("StageStatusManager");
+        StageStatusManager = FlagManager.GetComponent<TeamProject.StageStatusManager>();
+        rend = GetComponent<RawImage> ();
+
+  
+        rend.material.shader = Shader.Find("Unlit/Oil Painting");
+
+        //if(b.m_WatchOpeningFlag)
+        //{
+        //    this.gameObject.SetActive(false);
+        //}
+
+        //STAGE1-1突入フラグがtrueだったら動作させない
+        if(StageStatusManager.m_S1Flag)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        radius -= Time.deltaTime + Time.deltaTime + Time.deltaTime; //仮
+        if (time > 0)
+        {
+          
+            rend.material.SetFloat("_Radius", radius);
+        }
+        else
+        {
+            StageStatusManager.m_S1Flag = true;  //STAGE1-1突入フラグをtrueにし次回から実行させなくする
+            this.gameObject.SetActive(false);
+        }
+    }
+}
