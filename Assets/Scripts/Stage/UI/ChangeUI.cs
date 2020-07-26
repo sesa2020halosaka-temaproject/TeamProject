591af1debe60e9fd80e4bd2b0ae8b5281cea83e0
RyuichiMasuyama <57240248+RyuichiMasuyama@users.noreply.github.com>
2020-07-27 05:43:13 +0900

    UI動的切り替え

---
+++ Assets/Scripts/Stage/UI/ChangeUI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TeamProject
+{
+    public class ChangeUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Image[] images;
+
+        [SerializeField]
+        private Sprite[] keySprite;
+        [SerializeField]
+        private Sprite[] padSprite;
+        
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // 現在有効なパッドの種類を取得
+            var activePad = InputManager.InputManager.ActivePad;
+
+            if (activePad == InputManager.GamePad.Keyboad)
+            {
+                SetImage(keySprite);
+            }
+            else
+            {
+                SetImage(padSprite);
+            }
+        }
+
+        private void SetImage(Sprite[] _sprite)
+        {
+            for (int i = 0; i < images.Length; i++)
+            {
+                images[i].sprite = _sprite[i];
+            }
+        }
+    }
+}
