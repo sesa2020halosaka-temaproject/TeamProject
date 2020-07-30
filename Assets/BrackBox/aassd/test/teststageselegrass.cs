0a319fd1d91631b54ebdfcaa96ed82a8262902a3
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2020-07-31 06:17:21 +0900

    ・ステセレ上でステージ上の草が生えていないバグを修正したもの。

---
+++ Assets/BrackBox/aassd/test/teststageselegrass.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teststageselegrass : MonoBehaviour
+{
+    public GameObject a;
+    public bool flg,flg2;
+    float timeleft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flg = false;
+        flg2 = false;
+        timeleft = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft += Time.deltaTime;
+        if (timeleft >= 1.0)
+        {
+            if (flg2 == false)
+            {
+                if (flg == true)
+                {
+                    a.SetActive(true);
+                    flg = false;
+                    flg2 = true;
+                }
+                else
+                {
+                    a.SetActive(false);
+                    flg = true;
+                }
+            }
+            timeleft = 0.0f;
+        }
+    }
+}
